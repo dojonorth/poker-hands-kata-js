368acd0cec5ca3e3a3b255441b190fe7a097c373
Sunil Khutan <sunil.khutan@bbc.co.uk>
2017-01-20 16:45:11 +0000

    first commit

---
+++ test/rulesTest.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+
+describe('Poker Hand', function() {
+
+  it('should evaluate a hand as high card', function() {
+    expect(false).to.be.true;
+  });
+
+  it('should evaluate a hand as a pair', function() {
+    expect(false).to.be.true;
+  });
+
+});
